5c3695c537a793aaa4c8ce3e6ad484fb26c37774
mavrick w <mavwatts@gmail.com>
2020-04-06 20:04:30 -0400

    commiting jeopardy normal mode code

---
+++ jeopardy-normal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Jeopardy from './components/jeopardy/Jeopardy.js';
+
+
+function App() {
+  return (
+    <Jeopardy/>
+  );
+}
+
+export default App;
